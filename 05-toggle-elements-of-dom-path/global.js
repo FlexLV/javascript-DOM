0c9a722fdb002f1f4b6625f5eb82cbea2543096a
FlexLV <ernisd621@gmail.com>
2021-03-23 12:27:36 +0200

    Veiktas izmainas

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,54 @@
-// Your JavaScript goes here.
+document.addEventListener('DOMContentLoaded', function () {
+
+    const button = document.querySelector('#toggle_button')
+    const elements = document.querySelectorAll('.second_five > li')
+
+
+    var toggleStatus = false
+    button.addEventListener('click', function(e) {
+        if (toggleStatus == false)
+        {
+            elements.forEach(function(element) {
+                toggleStatus = true
+                element.style.display = 'flex'
+
+            })
+        }
+        else
+        {
+            elements.forEach(function (element) {
+                toggleStatus = false
+                element.style.display = 'none'
+            })
+        }
+    })
+
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const button = document.querySelector('#toggle_button')
+    const elements = document.querySelectorAll('.first_five > li')
+
+
+    var toggleStatus = false
+    button.addEventListener('click', function(e) {
+        if (toggleStatus == false)
+        {
+            elements.forEach(function(element) {
+                toggleStatus = true
+                element.style.display = 'none'
+
+            })
+        }
+        else
+        {
+            elements.forEach(function (element) {
+                toggleStatus = false
+                element.style.display = 'flex'
+            })
+        }
+    })
+
+})
+
--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,12 @@
-// Your JavaScript goes here.
+document.addEventListener('DOMContentLoaded', function () {
+    
+    
+    const nameBar = document.querySelector('#full_name')
+    var htmlName = document.querySelector('#greeting')
+    
+
+    nameBar.addEventListener('keyup', function (e) {
+        htmlName.textContent = "Hello there, " + e.target.value + "!"
+    })
+
+})
